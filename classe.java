4d266aef2bbe7f137ffe591925cb455ff39723b7
GessycaNobrega <145810565+GessycaNobrega@users.noreply.github.com>
2025-04-10 16:13:16 -0300

    Create classe.java

---
+++ classe.java
@@ -0,0 +1,16 @@
+/** O código a seguir mostra um exemplo de uma classe em Java: */
+
+public class Aluno {
+    private String nome; 
+    public void inserirNome(String nn) {
+        nome = nn;
+    }
+    public String recuperarNome() {
+        return nome;
+    }
+    public static void main(String args[]){
+        Aluno a = new Aluno(); 
+        a.inserirNome ("Pessoa");
+        System.out.println("saida: "+a.recuperarNome());
+    }
+} 
