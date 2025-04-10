6b6c83f5c8f4be7ac5c0f8856baa3c705fb43516
GessycaNobrega <145810565+GessycaNobrega@users.noreply.github.com>
2025-04-10 16:30:15 -0300

    Update Aluno.Java

--- Aluno.java
+++ Aluno.java
@@ -14,3 +14,22 @@ public class Aluno {
         System.out.println("saida: "+a.recuperarNome());
     }
 } 
+
+/*há duas formas de declaração de classes: Normal e Enum.
+
+A partir disso, vamos nos deter apenas à forma normal: */
+
+[Modificador] class Identificador [TipoParâmetros] [Superclasse] [Superinterface] { [Corpo da Classe] }
+
+/*um exemplo mais complexo que utiliza mais recursos de declaração de classe em Java: */
+
+@Deprecated @SuppressWarnings ("deprecation") public abstract strictfp class Aluno extends Pai implements Idealizacao, Sonho {
+    private String nome; 
+    public void inserirNome(String nn){
+        nome = nn;
+    }
+    public String recuperarNome(){
+       return nome;
+    }
+    ...//outros métodos ocultados por simplicidade    
+}
