f9c49ffaeed5bee226c36a31ca550f23178e0de4
GessycaNobrega <145810565+GessycaNobrega@users.noreply.github.com>
2025-04-14 17:38:45 -0300

    Create Alunoo.java

---
+++ Alunoo.java
@@ -0,0 +1,17 @@
+class Alunoo {
+    private String nome;
+    private int idade;
+    public Alunoo ( String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+    public void definirNome (String nome){
+        this.nome = nome;
+    }
+    public void definirIdade (int idade){
+        this.idade = idade;
+    }
+    public String recuperarNome () {
+        retur this.nome;
+    }
+}
