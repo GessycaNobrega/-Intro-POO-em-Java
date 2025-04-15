352d3b25f832a9e11dde3b54db92d70386e8bc2b
GessycaNobrega <145810565+GessycaNobrega@users.noreply.github.com>
2025-04-15 17:32:55 -0300

    create Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -1,35 +1,8 @@
-/** O código a seguir mostra um exemplo de uma classe em Java: */
-
-public class Aluno {
-    private String nome; 
-    public void inserirNome(String nn) {
-        nome = nn;
+public class Aluno extends Pessoa {
+    //Atributos    
+    private String matricula;
+    //Métodos
+    public Aluno(String nome, Calendar data_nascimento, long CPF, Endereco endereco){
+        super (nome, data_nascimento, CPF, endereco);
     }
-    public String recuperarNome() {
-        return nome;
-    }
-    public static void main(String args[]){
-        Aluno a = new Aluno(); 
-        a.inserirNome ("Pessoa");
-        System.out.println("saida: "+a.recuperarNome());
-    }
-} 
-
-/*há duas formas de declaração de classes: Normal e Enum.
-
-A partir disso, vamos nos deter apenas à forma normal: */
-
-[Modificador] class Identificador [TipoParâmetros] [Superclasse] [Superinterface] { [Corpo da Classe] }
-
-/*um exemplo mais complexo que utiliza mais recursos de declaração de classe em Java: */
-
-@Deprecated @SuppressWarnings ("deprecation") public abstract strictfp class Aluno extends Pai implements Idealizacao, Sonho {
-    private String nome; 
-    public void inserirNome(String nn){
-        nome = nn;
-    }
-    public String recuperarNome(){
-       return nome;
-    }
-    ...//outros métodos ocultados por simplicidade    
 }
