25d917eaf4a5154126932539dbf3c0f8dd429cfb
GessycaNobrega <145810565+GessycaNobrega@users.noreply.github.com>
2025-04-16 18:01:28 -0300

    update Diretor.java

--- Polimorfirmo/Diretor.java
+++ Polimorfirmo/Diretor.java
@@ -1,3 +1,6 @@
+import java.util.Calendar;
+import java.util.UUID;
+
 public class Diretor extends Empregado {
     //Métodos   
     public Diretor(String nome, Calendar data_nascimento, long CPF, Endereco endereco) {
@@ -6,4 +9,11 @@ public class Diretor extends Empregado {
     protected void gerarMatricula(){
          matricula = "E-" + UUID.randomUUID( ).toString( );  
     }
+    protected void alterarMatricula(){
+            gerarMatricula();
+    }
+    protected void alterarMatricula(String matricula){
+            this.matricula = matricula;
+    }
+}
  }
