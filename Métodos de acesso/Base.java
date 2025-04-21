81cc6d55444ee159f53435ff3133dddacc2ab5fc
GessycaNobrega <145810565+GessycaNobrega@users.noreply.github.com>
2025-04-21 17:30:29 -0300

    Create Base.Java

---
+++ Métodos de acesso/Base.java
@@ -0,0 +1,11 @@
+//Métodos de acesso
+
+public class Base {
+    private private nome;
+    public void setNome(String nome){    
+        this.nome=nome;
+    }
+    public String getNome(){    
+        return this.nome;
+    }
+} 
