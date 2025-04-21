f9f117f3ae1461c999f0cb6ce216a4f2f70f3ade
GessycaNobrega <145810565+GessycaNobrega@users.noreply.github.com>
2025-04-21 17:36:18 -0300

    Create switch.java

---
+++ Comando switch/switch.java
@@ -0,0 +1,14 @@
+switch ( < expressão> ) {
+    case < valor 1>:
+        bloco;
+        break;
+.
+.
+.
+    case :
+        bloco;
+    break;
+    default:        
+        bloco;
+             break;
+}
